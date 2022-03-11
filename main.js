90e29bb6349161828eeaeb227b48f38b5fdc012d
Singh970271 <97465033+Singh970271@users.noreply.github.com>
2022-03-11 22:55:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const result=require("./calculation.js")
+
+//result.addvalue(7,8)
+
+console.log(result.addvalue(7,8))
+
+console.log(result.div(24,8))
+
+console.log(result.mult(25,9))
+
+console.log(result .sub(500,250))
+
+
+
+
+
+
